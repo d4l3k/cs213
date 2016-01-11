c01ce016f735654e01e3b9d93a2888f7f402d0ea
Tristan Rice <rice@fn.lc>
2016-01-10 18:01:48 -0800

    Added assignment 1

---
+++ assignment1/code/Endianness.java
@@ -0,0 +1,42 @@
+import static java.lang.System.out;
+
+public class Endianness {
+
+  public static int bigEndianValue (Byte[] mem) {
+    int out = 0;
+    for (int i=0; i < mem.length; i++) {
+      out += mem[mem.length-i-1] * Math.pow(256, i);
+    }
+    return out;
+  }
+
+  public static int littleEndianValue (Byte[] mem) {
+    int out = 0;
+    for (int i=0; i < mem.length; i++) {
+      out += mem[i] * Math.pow(256, i);
+    }
+    return out;
+  }
+
+  public static void main (String[] args) {
+    Byte mem[] = new Byte[4];
+    try {
+      for (int i=0; i<4; i++)
+        mem [i] = Integer.valueOf (args[i], 16) .byteValue();
+    } catch (Exception e) {
+      out.printf ("usage: java Endianness n0 n1 n2 n3\n");
+      out.printf ("where: n0..n3 are byte values in memory at addresses 0..3 respectively, in hex (no 0x).\n");
+      return;
+    }
+
+    int bi = bigEndianValue    (mem);
+    int li = littleEndianValue (mem);
+
+    out.printf ("Memory Contents\n");
+    out.printf ("  Addr   Value\n");
+    for (int i=0; i<4; i++)
+      out.printf ("  %3d:   0x%-5x\n", i, mem[i]);
+    out.printf ("The big    endian integer value at address 0 is %d\n", bi);
+    out.printf ("The little endian integer value at address 0 is %d\n", li);
+  }
+}
