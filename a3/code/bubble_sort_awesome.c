1d8219a71dddae76797205332a42c8a8b3bdfac9
Tristan Rice <rice@fn.lc>
2016-01-23 15:43:54 -0800

    Finished bubble_sort_awesome

--- a3/code/bubble_sort_awesome.c
+++ a3/code/bubble_sort_awesome.c
@@ -5,13 +5,17 @@ int *val;
 
 void sort (int n) {
   int t;
-  for (int i=n-1; i>0; i--)
-    for (int j=i-1; j>=0; j--)
-      if (val[i] < val[j]) {
-        t = val[i];
-        val[i] = val[j];
-        val[j] = t;
+  for (int i=n-1; i>0; i--) {
+    int *ip = val + i * sizeof(int);
+    for (int j=i-1; j>=0; j--) {
+      int *jp = val + j * sizeof(int);
+      if (*ip < *jp) {
+        t = *ip;
+        *ip = *jp;
+        *jp = t;
       }
+    }
+  }
 }
 
 int main (int argc, char** argv) {
@@ -20,13 +24,16 @@ int main (int argc, char** argv) {
   n = argc - 1;
   val = malloc(n * sizeof(int));
   for (int i=0; i<n; i++) {
-    val[i] = strtol (argv[i+1], &ep, 10);
+    int *ip = val + i * sizeof(int);
+    *ip = strtol (argv[i+1], &ep, 10);
     if (*ep) {
       fprintf (stderr, "Argument %d is not a number\n", i);
       return -1;
     }
   }
   sort (n);
-  for (int i=0; i<n; i++)
-    printf ("%d\n", val[i]);
+  for (int i=0; i<n; i++) {
+    int *ip = val + i * sizeof(int);
+    printf ("%d\n", *ip);
+  }
 }
