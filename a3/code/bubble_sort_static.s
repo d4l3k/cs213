f49d36b26dee856a7b7792c8f488c978e5ec588d
Tristan Rice <rice@fn.lc>
2016-01-23 16:05:59 -0800

    Completed part 2 and part 3

---
+++ a3/code/bubble_sort_static.s
@@ -0,0 +1,23 @@
+.pos 0x100
+  ld $0x200, r0
+  ld (r0), r0
+  ld $0x204, r1
+  ld (r1), r1
+  ld $0x208, r2
+  ld (r2, r0, 4), r3
+  ld (r2, r1, 4), r4
+  st r3, (r2, r1, 4)
+  st r4, (r2, r0, 4)
+
+
+  halt
+
+.pos 0x200
+# Data area
+
+i:   .long 0
+j:   .long 0
+val: .long 1
+     .long 2
+     .long 3
+     .long 4
