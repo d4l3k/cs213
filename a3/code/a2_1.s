210a79cbcebda98ab3291149401b8b478292ea03
Tristan Rice <rice@fn.lc>
2016-01-26 13:41:20 -0800

    Solved syntax error

--- a3/code/a2_1.s
+++ a3/code/a2_1.s
@@ -7,7 +7,7 @@
   ld $0x10, r1 # 10
   ld (r0), r2  # c
   add r2, r1   # c + 10
-  ld 0x200, r3 # *b
+  ld $0x200, r3 # *b
   st r1, (r3)  # b = c + 10
 
   ld $0x208, r4      # *a
@@ -23,12 +23,12 @@
   ld (r4, r5, 4), r6 # a[8]
   ld (r3), r7        # b
   add r7, r6         # a[8] + b
-  ld (r0), r8        # c
   ld $0x7, r1        # 7
   and r1, r7         # b & 0x7
   ld (r4, r7, 4), r1 # a[b & 0x7]
   add r1, r6         # + ^
-  st r6, (r4, r8, 4) # a[c] = ^
+  ld (r0), r0        # c
+  st r6, (r4, r0, 4) # a[c] = ^
 
   halt
 
