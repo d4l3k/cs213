418abf38b59de1593e9dd366e6ed67ef9070b112
Tristan Rice <rice@fn.lc>
2016-01-31 14:38:56 -0800

    Added answers to Q2

--- a4/a4.s
+++ a4/a4.s
@@ -1,5 +1,8 @@
 .pos 0x1000
   code:
+
+  # init/malloc
+
   ld $heap0, r0 # r0 = &heap0
   ld $y, r1     # r1 = &s.y
   st r0, (r1)   # s.y = &heap0
@@ -7,6 +10,26 @@
   ld $heap1, r0 # r0 = &heap1
   ld $z, r1     # r1 = &s.z
   st r0, (r1)   # s.z = &ds
+
+  # foo()
+
+  ld $v, r0           # r0 = &v
+  ld $i, r1           # r1 = &i
+  ld (r1), r1         # r1 = i
+
+  ld $x, r2           # r2 = &x
+  ld (r2, r1, 4), r3  # r3 = x[i]
+  st r3, (r0)         # v = x[i]
+
+  ld $y, r2           # r2 = &y
+  ld (r2), r2         # r2 = y
+  ld (r2, r1, 4), r3  # r3 = y[i]
+  st r3, (r0)         # v = y[i]
+
+  ld $z, r2           # r2 = &z
+  ld (r2), r2         # r2 = z
+  ld (r2, r1, 4), r3  # r3 = z->x[0]
+  st r3, (r0)         # v = z->x[0]
 
   halt
 
