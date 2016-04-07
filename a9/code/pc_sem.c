03e72dba5560fbdde34463eb21e8e3a0ee619a7c
Tristan Rice <rice@fn.lc>
2016-04-06 21:31:12 -0700

    Finished problem 4

--- a9/code/pc_sem.c
+++ a9/code/pc_sem.c
@@ -14,13 +14,17 @@ int consumer_wait_count;     // # of times consumer had to wait
 int histogram [MAX_ITEMS+1]; // histogram [i] == # of times list stored i items
 
 struct Pool {
-  // TODO
+  uthread_sem_t lock;
+  uthread_sem_t notEmpty;
+  uthread_sem_t notFull;
   int           items;
 };
 
 struct Pool* createPool() {
   struct Pool* pool = malloc (sizeof (struct Pool));
-  // TODO
+  pool->lock = uthread_sem_create(1);
+  pool->notEmpty = uthread_sem_create(0);
+  pool->notFull = uthread_sem_create(MAX_ITEMS);
   pool->items     = 0;
   return pool;
 }
@@ -28,14 +32,34 @@ struct Pool* createPool() {
 void* producer (void* pv) {
   struct Pool* p = pv;
 
-  // TODO
+  for (int i=0; i<NUM_ITERATIONS; i++) {
+    producer_wait_count++;
+    uthread_sem_wait(p->notFull);
+
+    uthread_sem_wait(p->lock);
+    p->items++;
+    histogram [p->items] += 1;
+    assert (p->items <= MAX_ITEMS);
+    uthread_sem_signal(p->notEmpty);
+    uthread_sem_signal(p->lock);
+  }
   return NULL;
 }
 
 void* consumer (void* pv) {
   struct Pool* p = pv;
-  
-  // TODO
+
+  for (int i=0; i<NUM_ITERATIONS; i++) {
+    consumer_wait_count++;
+    uthread_sem_wait(p->notEmpty);
+
+    uthread_sem_wait(p->lock);
+    assert (p->items > 0);
+    p->items--;
+    histogram [p->items] += 1;
+    uthread_sem_signal(p->notFull);
+    uthread_sem_signal(p->lock);
+  }
   return NULL;
 }
 
@@ -44,9 +68,19 @@ int main (int argc, char** argv) {
 
   uthread_init (4);
   struct Pool* p = createPool();
-  
-  // TODO
-  
+
+  for (int i=0; i<NUM_CONSUMERS; i++) {
+    t[i] = uthread_create(&consumer, p);
+  }
+  for (int i=0; i<NUM_PRODUCERS; i++) {
+    t[i+NUM_CONSUMERS] = uthread_create(&producer, p);
+  }
+
+  for (int i=0; i<NUM_CONSUMERS+NUM_PRODUCERS; i++) {
+    void *res;
+    uthread_join(t[i], &res);
+  }
+
   printf ("producer_wait_count=%d, consumer_wait_count=%d\n", producer_wait_count, consumer_wait_count);
   printf ("items value histogram:\n");
   int sum=0;
